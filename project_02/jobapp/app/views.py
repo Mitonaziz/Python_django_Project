fabf67f544dc73099fa5761281ce1b68e1a040e7
Azizur <106616550+Mitonaziz@users.noreply.github.com>
2023-01-22 23:09:31 +0600

    create multiple job

--- project_02/jobapp/app/views.py
+++ project_02/jobapp/app/views.py
@@ -4,8 +4,10 @@ from django.http import HttpResponse
 def hello(requsest):
     return HttpResponse("<h1>hello word</h1>")
 def jobdetail(requsest, id):
-    return HttpResponse(f"<h1>Job details page {id}</h1>")
-
+   # return HttpResponse(f"<h1>Job details page {id}</h1>")
+    #make a site urls click
+    site="https://google.com"
+    return HttpResponse(f"visit <a href={site}>google here</a>")
 
 
 #"<domain>/job/1" -->job detail page
