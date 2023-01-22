11036cf83b2079c957b4d2cd5794908ca0b2d007
Azizur <106616550+Mitonaziz@users.noreply.github.com>
2023-01-22 20:10:02 +0600

    dainamic urls

--- project_02/jobapp/app/urls.py
+++ project_02/jobapp/app/urls.py
@@ -5,6 +5,6 @@ from app import views
 
 urlpatterns = [
     path('',views.hello),
-    path('job/1',views.jobdetail),
+    path('job/<id>',views.jobdetail),
 ]
- 
+  #dainamic  path('job/<id>',views.jobdetail),
--- project_02/jobapp/app/views.py
+++ project_02/jobapp/app/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def hello(requsest):
-    return HttpResponse("hello word")
-def jobdetail(requsest):
-    return HttpResponse("Job details page")
+    return HttpResponse("<h1>hello word</h1>")
+def jobdetail(requsest, id):
+    return HttpResponse(f"<h1>Job details page {id}</h1>")
 
 
 
