59f371d47da001a94343b05164fe392bb654081a
Azizur <106616550+Mitonaziz@users.noreply.github.com>
2023-01-24 00:28:34 +0600

    operators

--- python class work/work01.py
+++ python class work/work01.py
@@ -1,9 +1,13 @@
 # #operators
 # # + - / * (**)power {//}
+
+
 # numone = 4
 # numtwo = 2
 # div = (numone / numtwo )
 # print(div)
+
+
 # floDiv = (numone // numtwo)
 # print(floDiv)
 
@@ -13,14 +17,16 @@
 
 
 #taking user input in python
-NumOne = input()
+NumOne = input() # in this case python, all-time set the default value string so this two input take string value    
 Numtwo = input()
 additon = (NumOne + Numtwo)
-print(additon)
+print(additon) #sting valu sub
+
+
 FirstName = input("Enter your 1st name :" )
 secondname =input("Enter your 2nd name :")
-print(FirstName + secondname)
+print(FirstName + secondname) # sum tow name like FirstName= Azizur and secondname= rahman
 
-FirstNum =int (input("Enter your 1st naum :" ))
+FirstNum =int (input("Enter your 1st naum :" )) # take two integer type input and sum 
 secondnum =int ( input("Enter your 2nd naum :"))
 print(FirstNum + secondnum)
