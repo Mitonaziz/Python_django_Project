7f98c6b36725555740ddd59bf1567205e0ef90c2
Azizur <106616550+Mitonaziz@users.noreply.github.com>
2023-01-24 00:18:15 +0600

    input valu integer

--- python class work/work01.py
+++ python class work/work01.py
@@ -1,12 +1,26 @@
-#operators
-# + - / * (**)power {//}
-numone = 4
-numtwo = 2
-div = (numone / numtwo )
-print(div)
-floDiv = (numone // numtwo)
-print(floDiv)
+# #operators
+# # + - / * (**)power {//}
+# numone = 4
+# numtwo = 2
+# div = (numone / numtwo )
+# print(div)
+# floDiv = (numone // numtwo)
+# print(floDiv)
 
 
-expo = (numone ** numtwo)
-print(expo)
+# expo = (numone ** numtwo)
+# print(expo)
+
+
+#taking user input in python
+NumOne = input()
+Numtwo = input()
+additon = (NumOne + Numtwo)
+print(additon)
+FirstName = input("Enter your 1st name :" )
+secondname =input("Enter your 2nd name :")
+print(FirstName + secondname)
+
+FirstNum =int (input("Enter your 1st naum :" ))
+secondnum =int ( input("Enter your 2nd naum :"))
+print(FirstNum + secondnum)
