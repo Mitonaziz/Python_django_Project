f3598235b55e252b35bd9abc38f4d0cb47770fdd
Azizur <106616550+Mitonaziz@users.noreply.github.com>
2023-01-28 19:21:51 +0600

    calculato make

---
+++ python class work/work02Calculator.py
@@ -0,0 +1,21 @@
+ # + - / * (**)power {//}
+  
+FirstNumber =int ( input("Enter your First number = "))
+SecondNumber =int ( input("Enter your Second number = "))
+sum =int(FirstNumber+SecondNumber)
+print("Your Addition valu is = ",sum )
+
+div =int(FirstNumber/SecondNumber)
+print("Your divition valu is = ",div )
+
+mul=int(FirstNumber*SecondNumber)
+print("your Multiplication valu is = ",mul)
+
+sub= int(FirstNumber-SecondNumber)
+print("Subtraction valu is = ",sub)
+
+floor= int(FirstNumber//SecondNumber)
+print("Floor Division valu is = ",floor)
+
+power= int(FirstNumber**SecondNumber)
+print("power = ",power)
