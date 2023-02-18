b89e0e636cb3ef23aae230df9487c7d091418300
Md Azizur Rahman <106616550+Mitonaziz@users.noreply.github.com>
2023-02-18 13:13:36 +0600

    concateinetion

---
+++ python class work/w10Concatinetion.py
@@ -0,0 +1,8 @@
+first_name= "Aziuzr" 
+Last_naem = "Rahman"
+con = (" hellow My name is  " + first_name + " " + Last_naem + " "+"Miton" )
+print(con)
+
+age= 22
+print("My age is "+ str (age))
+    
