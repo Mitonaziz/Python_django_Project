d25f79281a27567c4b585a087110e957b4241fcd
Md Azizur Rahman <106616550+Mitonaziz@users.noreply.github.com>
2023-02-21 19:57:26 +0600

    if else

---
+++ python class work/w16logical_operation_if_else.py
@@ -0,0 +1,10 @@
+is_hungry = False
+is_thirsty =True
+if is_hungry and is_thirsty:
+    print("Eat and drink somthing")
+ 
+   #if is_hungry or is_thirsty:
+    #print("Eat and drink somthing")
+    #if is_hungry and not is_thirsty:
+        
+     #    print("Eat and drink somthing")
---
+++ python class work/w17ifelse_comperison.py
@@ -0,0 +1,7 @@
+num=4
+if(num>0):
+    print("the number is positive")
+elif num < 0 :
+    print("The number is negative")
+else:
+    print("the number is O.")
