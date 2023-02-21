92c73898e5e481451971970bba58f474e9119e99
Md Azizur Rahman <106616550+Mitonaziz@users.noreply.github.com>
2023-02-21 19:39:54 +0600

    If else elif

---
+++ python class work/w15_if_else.py
@@ -0,0 +1,11 @@
+good_weather = False
+bed_weather = False
+
+if good_weather :
+    print ("Go to the beach.")
+elif bed_weather:
+    print("Stay at home.")
+    print("Do homework")
+else:
+    print("Go on a walk. ")
+    
