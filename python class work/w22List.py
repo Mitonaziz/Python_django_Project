99d5e81fd3e337bfcd0aa2189889212d4ee81b24
Md Azizur Rahman <106616550+Mitonaziz@users.noreply.github.com>
2023-03-01 21:43:15 +0600

    list

--- python class work/w22List.py
+++ python class work/w22List.py
@@ -1,8 +1,13 @@
 #list
 Name =["Azizur","Nasim","emon","Sajed",]
 print(Name)
+print(Name [1:3])#print 1 and 3
+print(Name [1:]) #2nd to last print
+print(Name [:3])#1st to 3 pirnt
+
 id= ["Azizur",14,"Namim",30]
 print(id)
 print(id[1])
 id[1]=20
 print(id)
+print(len(id))
